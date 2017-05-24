201e7df156d0d0b9ca514e66eb207b5e831f0624
Shafaiatul Islam <shafaiatul@gmail.com>
2017-05-23 22:29:03 -0400

    Add detail.json and implement it

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ angular.module("SelectFilter", [ "kendo.directives", "checklist-model" ])
                 categories: []
             }
         }
+        $scope.checkedItem = {
+            selectedItem: {
+                expertises: []             
+            }
+        }
         
     
-        // Getting all the data from the JSON file
-        $http.get('data.json').then(function(response) {
+        // Getting all the data from the background.json file
+        //-----------------------------------------------------
+        $http.get('data/jobs.json').then(function(response) {
             $scope.jobs = response.data;
 
             $scope.selectOptions = {
@@ -23,12 +29,39 @@ angular.module("SelectFilter", [ "kendo.directives", "checklist-model" ])
                 optionLabel: "Select..."
             }
             
-        })
+        });
+
+        // Mapping background.json data with the data from detail.json
+        //--------------------------------------------------------------        
+        function getDetail() {
+            $http.get('data/detail.json').then(function(response) {
+                $scope.details = response.data;
+            
+                $scope.details.forEach(function(jobDetail) {
+                    //console.log(jobDetail.name);
+                    
+                        //console.log(job.name);
+                        if(jobDetail.id === $scope.selectedItem.id) {
+             
+                            $scope.selectedItem.expertises = jobDetail.expertises;
+                            console.log($scope.selectedItem.expertises);
+                            $scope.selectedItem.expertises.forEach(function(eachExpertise) {
+                                console.log(eachExpertise.field);
+                            });
+                        }
+                    
+                
+                });
+            });
+        } 
+         
         
         
         // Selecting a Item from Dropdownlist
+        //-------------------------------------
         $scope.change = function(e) {
             $scope.checkedItem.selectedItem.categories = [];
+            $scope.checkedItem.selectedItem.expertises = [];
             $scope.finalInfo = {};
             
             $scope.itemChosen = true;
@@ -36,25 +69,40 @@ angular.module("SelectFilter", [ "kendo.directives", "checklist-model" ])
             if(e.sender.text() === 'Select...'){
                 $scope.itemChosen = false;
                 console.log($scope.itemChosen)
-            }         
+            } 
+            getDetail();
+            
+
         }
 
            
-        // Check All and UnChcek All button
-        $scope.checkAll = function() {
-            $scope.checkedItem.selectedItem.categories = $scope.selectedItem.categories.map(function(item) { return item.id; });
+        // Check All and UnChcek All button for Categories
+        //------------------------------------------------
+        $scope.checkAllCategories = function() {
+            $scope.checkedItem.selectedItem.categories = angular.copy($scope.selectedItem.categories);
         };
-        $scope.uncheckAll = function() {
+        $scope.uncheckAllCategories = function() {
             $scope.checkedItem.selectedItem.categories = [];
+        };
+
+        // Check All and UnChcek All button for Expertises
+        //------------------------------------------------
+        $scope.checkAllExpertise = function() {
+            $scope.checkedItem.selectedItem.expertises = angular.copy($scope.selectedItem.expertises);
+        };
+        $scope.uncheckAllExpertise = function() {
+            $scope.checkedItem.selectedItem.expertises = [];
         };
            
         
         // Send the checked items under a selected item 
+        //----------------------------------------------
         $scope.checkedItemSend = function() {
             $scope.finalInfo = {
-                id: $scope.selectedItem.id,
-                name: $scope.selectedItem.name,
-                chosenCategories: $scope.checkedItem.selectedItem.categories
+                Id: $scope.selectedItem.id,
+                Name: $scope.selectedItem.name,
+                ChosenCategories: $scope.checkedItem.selectedItem.categories,
+                ChosenExpertises: $scope.checkedItem.selectedItem.expertises
             }
         }
 })
